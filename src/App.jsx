2cc89eca7bdca402543fb4d31b1f0ca51bb531fb
hyeoki <dlagogur93@gmail.com>
2022-10-16 21:15:16 +0900

    feature : dev env proxy settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import GlobalStyles from './globalStyles';
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://7c5a-219-255-199-146.jp.ngrok.io',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: true,
+        ws: true,
+      },
+    },
+  },
+});
