0416db56c834bc37b83ce035858e8fc0385869e1
hyeoki <dlagogur93@gmail.com>
2022-10-17 01:13:13 +0900

    feature : test

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -16,23 +16,33 @@ export const TestPage = () => {
   return (
     <div>
       <div>
-        <input value={id} onChange={(e) => setId(e.target.value)} key="id" />
+        <div>
+          id :{' '}
+          <input value={id} onChange={(e) => setId(e.target.value)} key="id" />
+        </div>
+        password :
         <input
           value={password}
           onChange={(e) => setPw(e.target.value)}
           key="pw"
         />
+      </div>
+      <div>
+        nickname :
         <input
           value={nickname}
           onChange={(e) => setNickname(e.target.value)}
           key="nk"
         />
       </div>
-      <input
-        value={roadmapName}
-        onChange={(e) => setRoadmapName(e.target.value)}
-        key="nk"
-      />
+      <div>
+        roadmapName :
+        <input
+          value={roadmapName}
+          onChange={(e) => setRoadmapName(e.target.value)}
+          key="nk"
+        />
+      </div>
       <div>
         <button
           onClick={() =>
@@ -88,7 +98,7 @@ export const TestPage = () => {
             )
           }
         >
-          register
+          save Roadmap
         </button>
       </div>
     </div>
