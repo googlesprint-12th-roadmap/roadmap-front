563ee54789ceedd37c24d2b205277217344c4d06
OH-JOO-YEONG <brb1111@naver.com>
2022-10-17 21:58:02 +0900

    Fix: 폴더명 변경

---
+++ src/components/make/NodeOption.jsx
@@ -0,0 +1,297 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import styled from 'styled-components';
+import {
+  depthState,
+  nodeListState,
+  currentSelectedIdState,
+} from '../../atoms/makeListAtoms';
+import { titleState } from '../../atoms/createAtoms';
+import { useSaveRoadmap } from '../../hooks/useRoadmap';
+import { useNavigate } from 'react-router-dom';
+
+const NodeOption = () => {
+  const navigate = useNavigate();
+  const [currentTitle, setCurrentTitle] = useState('');
+  const [currentType, setCurrentType] = useState('');
+  const [currentUrl, setCurrentUrl] = useState('');
+  const [currentDesc, setCurrentDesc] = useState('');
+
+  const [nodeList, setNodeList] = useRecoilState(nodeListState);
+  const [depthList, setDepthList] = useRecoilState(depthState);
+  const [currentSelectedId, setCurrentSelectedId] = useRecoilState(
+    currentSelectedIdState,
+  );
+  const [title, setTitle] = useRecoilState(titleState);
+
+  const saveRoadmap = useSaveRoadmap();
+
+  useEffect(() => {
+    if (currentSelectedId === -1) {
+      setCurrentTitle('');
+      setCurrentUrl('');
+      setCurrentDesc('');
+      setCurrentType('');
+    } else {
+      const currentNode = nodeList.find(
+        (item) => item.idx === currentSelectedId,
+      );
+      if (currentNode) {
+        setCurrentTitle(currentNode.title);
+        setCurrentUrl(currentNode.url);
+        setCurrentDesc(currentNode.desc);
+        setCurrentType(currentNode.type);
+      }
+    }
+  }, [currentSelectedId]);
+
+  // 노드 옵션 수정 함수
+  const handleNodeUpdate = () => {
+    if (!currentTitle.length) {
+      return alert('노드의 제목을 입력해주세요.');
+    }
+    if (currentSelectedId === -1) {
+      return alert('노드를 선택해주세요.');
+    }
+    const newNodeList = nodeList.map((item) => {
+      if (item.idx === currentSelectedId) {
+        return {
+          ...item,
+          title: currentTitle,
+          desc: currentDesc,
+          url: currentUrl,
+          type: currentType,
+        };
+      }
+      return item;
+    });
+    setNodeList(newNodeList);
+
+    const newDepthList = depthList.map((item1) => {
+      return item1.map((item2) => {
+        if (item2.idx === currentSelectedId) {
+          return {
+            ...item2,
+            title: currentTitle,
+            desc: currentDesc,
+            url: currentUrl,
+            type: currentType,
+          };
+        }
+        return item2;
+      });
+    });
+    setDepthList(newDepthList);
+  };
+
+  // 선택한 노드 삭제 함수
+  // 추후 더 간결한 코드로 수정 필요
+  const handleNodeDelete = () => {
+    if (currentSelectedId === -1) {
+      return alert('노드를 선택해주세요.');
+    }
+    const SelectedNode = nodeList.find(
+      (item) => item.idx === currentSelectedId,
+    );
+    if (SelectedNode.idx === SelectedNode.parent) {
+      return alert('루트 노드는 삭제할 수 없습니다.');
+    }
+    if (
+      window.confirm(
+        '선택한 노드의 하위 노드까지 전부 삭제됩니다. 진행 하시겠습니까?',
+      )
+    ) {
+      // 연쇄적인 children을 계속 탐색하는 로직
+      const toBeDeleted = [];
+      let queue = [];
+      queue.push(currentSelectedId);
+
+      while (queue.length) {
+        let currentId = queue.shift();
+        let currentNode = nodeList.find((item) => item.idx === currentId);
+        queue = [...queue, ...currentNode.children];
+        toBeDeleted.push(currentId);
+      }
+
+      // 부모 노드의 children 배열 수정
+      const parentId = SelectedNode.parent;
+
+      const newNodeList = nodeList
+        .filter((item) => !toBeDeleted.includes(item.idx))
+        .map((x) => {
+          if (x.idx === parentId) {
+            const newChildren = x.children.filter(
+              (y) => y !== currentSelectedId,
+            );
+            return { ...x, children: newChildren };
+          }
+          return x;
+        });
+
+      setNodeList(newNodeList);
+      const newDepthList = depthList.map((item1) =>
+        item1
+          .filter((item2) => !toBeDeleted.includes(item2.idx))
+          .map((x) => {
+            if (x.idx === parentId) {
+              const newChildren = x.children.filter(
+                (y) => y !== currentSelectedId,
+              );
+              return { ...x, children: newChildren };
+            }
+            return x;
+          }),
+      );
+      setDepthList(newDepthList);
+    }
+  };
+
+  // 로드맵 저장 기능
+  const handleSubmit = () => {
+    const context = {
+      name: title,
+      nodes: nodeList,
+      rootIdx: nodeList[0].idx,
+    };
+    console.log(context);
+    saveRoadmap.mutate(context, {
+      onSuccess: (d) => {
+        console.log(d);
+        navigate('/view/' + d.data.idx);
+      },
+    });
+  };
+
+  return (
+    <Container>
+      <EditNode>
+        <Title>
+          Edit Node
+          <Button onClick={handleNodeUpdate}>변경 내용 저장</Button>
+        </Title>
+        <ContentWrapper>
+          <Content>
+            <ContentName>제목</ContentName>
+            <ContentInput
+              placeholder="제목을 입력해주세요"
+              value={currentTitle}
+              onChange={(e) => setCurrentTitle(e.target.value)}
+            ></ContentInput>
+            <Button
+              isSelected={currentType === 'MAIN'}
+              onClick={() => setCurrentType('MAIN')}
+            >
+              메인
+            </Button>
+            <Button
+              isSelected={currentType === 'SUB'}
+              onClick={() => setCurrentType('SUB')}
+            >
+              서브
+            </Button>
+          </Content>
+          <Content>
+            <ContentName>관련 URL</ContentName>
+            <ContentInput
+              placeholder="URL을 입력해주세요"
+              value={currentUrl}
+              onChange={(e) => setCurrentUrl(e.target.value)}
+            ></ContentInput>
+          </Content>
+          <Content>
+            <ContentName>설명</ContentName>
+            <ContentInput
+              placeholder="해당 내용에 대해 자세한 설명을 입력해주세요."
+              value={currentDesc}
+              onChange={(e) => setCurrentDesc(e.target.value)}
+            ></ContentInput>
+          </Content>
+        </ContentWrapper>
+      </EditNode>
+      <Options>
+        <Title>Options</Title>
+        <ContentWrapper>
+          <Button onClick={handleNodeDelete}>선택한 노드 삭제</Button>
+          <Button
+            onClick={() => {
+              navigate('/view');
+              console.log('미리보기 클릭');
+            }}
+          >
+            미리보기
+          </Button>
+          <Button onClick={handleSubmit}>로드맵 저장</Button>
+        </ContentWrapper>
+      </Options>
+    </Container>
+  );
+};
+
+export default NodeOption;
+
+const Container = styled.section`
+  display: flex;
+
+  margin-right: 5rem;
+  justify-content: space-between;
+`;
+const EditNode = styled.div`
+  width: 40%;
+`;
+
+const Options = styled.div`
+  width: 40%;
+`;
+
+const Title = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 2.4rem;
+  font-weight: 700;
+
+  padding-bottom: 1rem;
+  border-bottom: 1px solid #000;
+`;
+
+const ContentWrapper = styled.div`
+  margin-top: 3rem;
+`;
+const Content = styled.div`
+  display: flex;
+  margin-bottom: 2.4rem;
+  align-items: center;
+`;
+const ContentName = styled.div`
+  min-width: 10rem;
+  font-size: 2.4rem;
+  font-weight: 400;
+`;
+const ContentInput = styled.input`
+  min-height: 35px;
+  max-width: 60rem;
+  flex-grow: 1;
+  padding: 0 1rem 0 1rem;
+
+  border: none;
+  border-radius: 10px;
+
+  background-color: #efefef;
+`;
+
+const Button = styled.button`
+  padding: 5px 15px 5px 15px;
+  margin-left: 5px;
+  margin-right: 5px;
+  border: none;
+  border-radius: 10px;
+
+  font-size: 16px;
+  font-weight: 600;
+
+  background-color: ${(props) => (props.isSelected ? '#ff639b' : '#ffa4c5')};
+  :hover {
+    background-color: #ff639b;
+  }
+  color: white;
+  cursor: pointer;
+`;
---
+++ src/components/make/SelectList.jsx
@@ -0,0 +1,202 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import styled, { css } from 'styled-components';
+import {
+  currentSelectViewState,
+  depthState,
+  emptyNodeCheckState,
+  lastSelectIdState,
+  nodeListState,
+  currentSelectedIdState,
+} from '../../atoms/makeListAtoms';
+import SelectItem from './SelectItem';
+import _ from 'lodash';
+import { AddNodeButton, AddNodeForm, AddNodeWrap } from './make.styles';
+
+const SelectList = ({ data, depth }) => {
+  const [isShown, setIsShown] = useState(false);
+  // recoil 전역 상태로 변경
+  const [currentCheckId, setCurrentCheckId] = useRecoilState(
+    currentSelectedIdState,
+  );
+  const [nodeList, setNodeList] = useRecoilState(nodeListState);
+  const [depthList, setDepthList] = useRecoilState(depthState);
+  const [emptyNode, setEmptyNode] = useRecoilState(emptyNodeCheckState);
+
+  const [currentSelect, setCurrentSelect] = useRecoilState(
+    currentSelectViewState,
+  );
+  const [lastId, setLastId] = useRecoilState(lastSelectIdState);
+
+  // 로빈
+  const [editing, setEditing] = useState(false);
+  const [newNodeText, setNewNodeText] = useState('');
+
+  const addNodeInputRef = useRef(null);
+
+  const handleClickSelectItem = useCallback(
+    (idx, index) => {
+      const tempDepthList = _.cloneDeep(depthList);
+      setCurrentSelect(depth);
+
+      if (data[index].idx === currentCheckId) {
+        setCurrentCheckId(-1);
+        setEmptyNode(false);
+        setDepthList([...tempDepthList.slice(0, depth + 1)]);
+        return;
+      }
+      setCurrentCheckId(idx);
+
+      if (data[index].children.length === 0) {
+        setEmptyNode(true);
+        setLastId(idx);
+        setDepthList([...tempDepthList.slice(0, depth + 1)]);
+        return;
+      }
+
+      const tempCurrenDepth = [...data[index].children].map(
+        (idx) => nodeList.filter((item) => item.idx === idx)[0],
+      );
+      setEmptyNode(false);
+
+      setDepthList(tempDepthList.slice(0, depth + 1).concat([tempCurrenDepth]));
+    },
+    [currentCheckId, depthList],
+  );
+
+  // 로빈
+  const handleAddNode = (e) => {
+    e.preventDefault();
+
+    if (newNodeText.length === 0) {
+      return alert('노드의 이름을 적어주세요!');
+    }
+
+    const newNode = {
+      idx: new Date().getTime(),
+      url: '',
+      desc: '',
+      type: 'MAIN',
+      title: newNodeText,
+      children: [],
+      parent: data[0].parent,
+    };
+
+    setNewNodeText('');
+    setEditing(false);
+    setNodeList([
+      ...nodeList.map((node) =>
+        node.idx === newNode.parent
+          ? { ...node, children: [...node.children, newNode.idx] }
+          : node,
+      ),
+      newNode,
+    ]);
+    console.log(newNode);
+
+    let tempDepthList = _.cloneDeep(depthList);
+
+    tempDepthList[depth] = [...data, newNode];
+    const depthIds = tempDepthList[depth].map((node) => node.idx);
+    const found = tempDepthList[depth - 1].map((node) =>
+      node.idx === data[0].parent ? { ...node, children: depthIds } : node,
+    );
+    tempDepthList[depth - 1] = found;
+
+    setDepthList(tempDepthList);
+  };
+
+  return (
+    <Container>
+      <ListContainer
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+        isShown={isShown}
+      >
+        <ul>
+          {data &&
+            data.length > 0 &&
+            data.map((item, index) => {
+              return (
+                <SelectItem
+                  key={item.idx + item.title}
+                  data={item}
+                  check={currentCheckId === item.idx}
+                  clickFunc={handleClickSelectItem}
+                  index={index}
+                />
+              );
+            })}
+        </ul>
+      </ListContainer>
+      {depth > 0 && (
+        <AddNodeWrap>
+          {editing ? (
+            <AddNodeForm>
+              <form onSubmit={handleAddNode}>
+                <input
+                  onChange={(e) => setNewNodeText(e.target.value)}
+                  type="text"
+                  value={newNodeText}
+                  ref={addNodeInputRef}
+                />
+                <button type="submit">추가</button>
+              </form>
+              <button
+                onClick={() => {
+                  setEditing(false);
+                  setNewNodeText('');
+                }}
+              >
+                취소
+              </button>
+            </AddNodeForm>
+          ) : (
+            <AddNodeButton
+              onClick={() => {
+                setEditing(true);
+                setTimeout(() => {
+                  addNodeInputRef?.current?.focus();
+                }, 100);
+              }}
+            >
+              <div></div>
+              <p>노드를 추가해주세요!</p>
+            </AddNodeButton>
+          )}
+        </AddNodeWrap>
+      )}
+    </Container>
+  );
+};
+
+export default SelectList;
+
+const Container = styled.div`
+  padding-left: 1rem;
+  padding-right: 0.5rem;
+
+  padding-bottom: 1rem;
+`;
+
+const ListContainer = styled.div`
+  min-width: 50rem;
+  height: 35rem;
+  overflow-y: scroll;
+
+  padding-left: 1rem;
+  padding-right: 0.5rem;
+  padding-top: 1rem;
+
+  ::-webkit-scrollbar-thumb {
+    background-color: transparent;
+  }
+
+  ${(props) =>
+    props.isShown &&
+    css`
+      ::-webkit-scrollbar-thumb {
+        background: #d9d9d9;
+      }
+    `}
+`;
